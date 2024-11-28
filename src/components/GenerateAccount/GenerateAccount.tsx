dd4284a7eb3cd275e252d01576bff834f4ddc6b7
athevinha <nguyenthevinh1002@gmail.com>
2024-11-28 13:45:19 +0700

    generate account ui

--- src/components/GenerateAccount/GenerateAccount.tsx
+++ src/components/GenerateAccount/GenerateAccount.tsx
@@ -1,32 +1,28 @@
-import {Cell,Image,List,Section} from '@telegram-apps/telegram-ui';
-import {useEffect, useMemo, type FC} from 'react';
+import { ButtonCell, Cell, List, Section } from "@telegram-apps/telegram-ui";
+import { useEffect, type FC } from "react";
 
-import {Page} from '@/components/Page.tsx';
-import {useWeb3Account} from './hook/useWeb3Account';
-
+import { Page } from "@/components/Page.tsx";
+import { useWeb3Account } from "./hook/useWeb3Account";
 
 export const GenerateAccount: FC = () => {
-    const {generateWeb3Account} = useWeb3Account()
-    useEffect(() => {
-        console.log(generateWeb3Account())
-        // return {evmWallet, btcWallet}
-    },[])
+  const { generateWeb3Account } = useWeb3Account();
+  useEffect(() => {
+    console.log(generateWeb3Account());
+  }, []);
   return (
     <Page back={false}>
       <List>
-        <Section
-          header="Generate Accounts"
-          footer="Endurio.io"
-          >
-            <Cell
-              subtitle="Generate Wallet With ARB & BTC mainnet"
-            >
-                {/* {JSON.stringify(btcWallet)} */}
-                {/* {JSON.stringify(evmWallet)} */}
-                {/* Generate Account Component */}
-            </Cell>
+        <Section header="Generate Accounts"
+          footer="Endurio.io">
+          <ButtonCell before="[ARB & BTC]">Generate Account</ButtonCell>
+          <Cell>
+
+          </Cell>
         </Section>
+
       </List>
     </Page>
   );
 };
+
+
