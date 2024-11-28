766dd7252e6fc591bdd06348488a7c778d49b487
athevinha <nguyenthevinh1002@gmail.com>
2024-11-28 13:44:42 +0700

    add context global state

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -1,20 +1,31 @@
-import { Section, Cell, Image, List, Button, ButtonCell } from '@telegram-apps/telegram-ui';
-import type { FC } from 'react';
-import { Link } from '@/components/Link/Link.tsx';
-import { Page } from '@/components/Page.tsx';
-import {init,postEvent} from "@telegram-apps/sdk";
+import { Page } from "@/components/Page.tsx";
+import { init, postEvent } from "@telegram-apps/sdk";
+import { List } from "@telegram-apps/telegram-ui";
+import { createContext, useContext, useState, type FC } from "react";
 
-import tonSvg from './ton.svg';
-import {GenerateAccount} from '@/components/GenerateAccount/GenerateAccount';
+import { GenerateAccount } from "@/components/GenerateAccount/GenerateAccount";
+import { IWeb3Account } from "@/type";
+import {useInitWeb3Account} from "@/components/GenerateAccount/hook/useWeb3Account";
 
 init();
 postEvent("web_app_setup_swipe_behavior", { allow_vertical_swipe: false });
+interface AppContextType {
+  web3Account: IWeb3Account | undefined;
+  setWeb3Account: React.Dispatch<
+    React.SetStateAction<IWeb3Account | undefined>
+  >;
+}
+
+const AppContext = createContext<AppContextType | undefined>(undefined);
 
 export const IndexPage: FC = () => {
+  const [web3Account, setWeb3Account] = useState<IWeb3Account>();
+  useInitWeb3Account()
   return (
-    <Page back={false}>
-      <List>
-        <Section
+    <AppContext.Provider value={{ web3Account, setWeb3Account }}>
+      <Page back={false}>
+        <List>
+          {/* <Section
           header="Features"
           footer="You can use these pages to learn more about features, provided by Telegram Mini Apps and other useful projects"
         >
@@ -26,15 +37,13 @@ export const IndexPage: FC = () => {
               TON Connect
             </Cell>
           </Link>
-        </Section>
-        <Section
+        </Section> */}
+          <GenerateAccount />
+
+          {/* <Section
           header="Application Launch Data"
-          footer="These pages help developer to learn more about current launch information"
-        >
-          <ButtonCell>
-            Generate Wallet [ARB & BTC]
-          </ButtonCell>
-          <GenerateAccount/>
+          footer="Endurio.io"
+        > */}
           {/* <Link to="/init-data">
             <Cell subtitle="User data, chat information, technical data">Init Data</Cell>
           </Link>
@@ -44,8 +53,19 @@ export const IndexPage: FC = () => {
           <Link to="/theme-params">
             <Cell subtitle="Telegram application palette information">Theme Parameters</Cell>
           </Link> */}
-        </Section>
-      </List>
-    </Page>
+          {/* </Section> */}
+        </List>
+      </Page>
+    </AppContext.Provider>
   );
+};
+
+
+
+export const useAppContext = () => {
+  const context = useContext(AppContext);
+  if (!context) {
+    throw new Error("useAppContext must be used within an AppProvider");
+  }
+  return context;
 };
