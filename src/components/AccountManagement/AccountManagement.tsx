1db58057251f78f847f3c93f6363de9e25582c19
athevinha <nguyenthevinh1002@gmail.com>
2024-11-28 16:03:10 +0700

    feat: add deposit and withdraw

--- src/components/AccountManagement/AccountManagement.tsx
+++ src/components/AccountManagement/AccountManagement.tsx
@@ -1,18 +1,18 @@
 import {
-  ButtonCell,
-  Card,
+  Button,
   Cell,
-  Text,
   List,
-  Section,
+  Section
 } from "@telegram-apps/telegram-ui";
-import { useContext, useEffect, type FC } from "react";
+import {useContext,type FC} from "react";
 
-import { Page } from "@/components/Page.tsx";
-import { useWeb3Account } from "./hook/useWeb3Account";
-import { AppContext } from "@/pages/IndexPage/IndexPage";
-import { shortenAddress } from "@/utils/utils";
-
+import {Page} from "@/components/Page.tsx";
+import {AppContext} from "@/pages/IndexPage/IndexPage";
+import {shortenAddress} from "@/utils/utils";
+// import {Iconify} from "../iconify";
+import '@/pages/IndexPage/IndexPage.css';
+import {Iconify} from "../iconify";
+import {ButtonBase} from "@mui/material";
 export const AccountManagement: FC = () => {
   const { setWeb3Account, web3Account } = useContext(AppContext);
 
@@ -27,6 +27,11 @@ export const AccountManagement: FC = () => {
               <div>BTC: {shortenAddress(web3Account?.btcAddress)}</div>
             </List>
           </Cell>
+          <div>
+            <Button before={<Iconify icon='material-symbols:download'/>} className='w-50'>Deposit</Button>
+            <Button before={<Iconify icon='material-symbols:upload'/>} className='w-50'>Withdraw</Button>
+          </div>
+
         </Section>
       </List>
     </Page>
