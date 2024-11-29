8e15cd6d10a40e02344852b9ec3a7a8e54c20373
athevinha <nguyenthevinh1002@gmail.com>
2024-11-29 10:15:12 +0700

    init whitelist token

---
+++ src/utils/constant.ts
@@ -0,0 +1,5 @@
+export const WHITELIST_TOKEN = {
+    USDC: '0xff970a61a04b1ca14834a43f5de4533ebddb5cc8',
+    USDT: '0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9',
+    ETH: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE'
+}
