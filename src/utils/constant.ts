b7ec2967d09229411241fbe10d51bd44a656de78
athevinha <nguyenthevinh1002@gmail.com>
2024-11-29 10:19:35 +0700

    feat: multicall

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -3,3 +3,4 @@ export const WHITELIST_TOKEN = {
     USDT: '0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9',
     ETH: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE'
 }
+export const MULTICALL_ADDRESS = '0xcA11bde05977b3631167028862bE2a173976CA11'
---
+++ src/utils/multicall.ts
@@ -0,0 +1,28 @@
+import {
+  ContractCallContext,
+  Multicall,
+} from "ethereum-multicall";
+import { Provider } from '@ethersproject/providers';
+
+export const multicall = async(
+  ethersProvider: Provider,
+  contexts: ContractCallContext[],
+  tryAggregate: boolean = true,
+): Promise<any[]> => {
+  const callbacks: { [reference: string]: any } = {}
+  for (const context of contexts) {
+    if (callbacks[context.reference]) {
+      throw new Error(`dupplicated reference: ${context.reference}`)
+    }
+    callbacks[context.reference] = context.context
+    delete context.context
+  }
+  const multicall = new Multicall({ ethersProvider, tryAggregate });
+  const { results } = await multicall.call(contexts);
+  return Object.values(results).map(result => {
+    const callback = callbacks[result.originalContractCallContext.reference]
+    if (callback != null && typeof callback === 'function') {
+      return callback(result.callsReturnContext)
+    }
+  })
+}
